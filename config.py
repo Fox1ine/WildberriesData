fe74353e6122f4b17c422a13f40834c501ff9b9d
Fox1ine <bopavelb@gmail.com>
2024-08-10 21:35:33 +0300

    Corrected almost all the inaccuracies

---
+++ config.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36",
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
+}
+
+service = Service(GeckoDriverManager().install())
+options = webdriver.FirefoxOptions()
+options.add_argument('--headless')
--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 import time
 from bs4 import BeautifulSoup
 import pandas as pd
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36",
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
-}
-
-
-service = Service('D:/FoxDriver/geckodriver.exe')
-options = webdriver.FirefoxOptions()
-options.add_argument('--headless')
+from config import headers, service, options
 
 
 def get_content(url):
@@ -23,9 +11,9 @@ def get_content(url):
         driver.get(url)
         time.sleep(5)
 
-        with open("Wild.html", 'w', encoding='utf-8') as file:
-            file.write(driver.page_source)
-        print("HTML was successfully loaded into 'Wild.html'")
+        page_sourse = driver.page_source
+        print("HTML content successfully loaded")
+        return page_sourse
 
     except Exception as err:
         print("Something went wrong", err)
@@ -33,15 +21,8 @@ def get_content(url):
         driver.quit()
 
 
-def get_item_urls(file_path):
-    try:
-        with open(file_path, 'rb') as file:
-            src = file.read().decode('utf-8', errors='ignore')
-    except UnicodeDecodeError as e:
-        print(f"Ошибка декодирования: {e}")
-        return []
-
-    soup = BeautifulSoup(src, 'lxml')
+def get_item_urls(html_content):
+    soup = BeautifulSoup(html_content, 'lxml')
     item_divs = soup.findAll('div', class_='product-card__wrapper')
 
     urls = []
@@ -55,18 +36,11 @@ def get_item_urls(file_path):
                 item_url = f"https://www.wildberries.ru{item_url}"
             urls.append(item_url)
 
-    with open("Wild.html", 'w', encoding='utf-8') as file:
-        for url in urls:
-            file.write(f"{url}\n")
-
     print("Urls collected successfully")
     return urls
 
 
-def get_data(fileURL_path):
-    with open(fileURL_path) as file:
-        urls_list = [url.strip() for url in file.readlines()]
-
+def get_data(urls_list):
     driver = webdriver.Firefox(service=service, options=options)
     result_list = []
 
@@ -116,7 +90,8 @@ def get_data(fileURL_path):
                 print(f"That product doesn't have price")
 
             images_urls = []
-            item_image = soup.find('div', class_ = 'zoom-image-container').find('img', class_ = 'photo-zoom__preview j-zoom-image hide')
+            item_image = soup.find('div', class_='zoom-image-container').find('img',
+                                                                              class_='photo-zoom__preview j-zoom-image hide')
             if item_image:
                 item_image = item_image.get('src')
                 images_urls.append(item_image)
@@ -141,21 +116,21 @@ def get_data(fileURL_path):
 
     # to csv
     df = pd.DataFrame(result_list)
-    df.to_csv("result.csv", index=False, encoding='utf-8-sig')
-    print("Data successfully saved to 'result.csv'")
+    df.to_csv("result.csv", index=False, encoding='utf-8')
     print("Data successfully saved to 'result.csv'")
 
 
 def parse_connect(user_value: str):
     url = f"https://www.wildberries.ru/catalog/0/search.aspx?search={user_value}"
-    get_content(url)
+    html_content = get_content(url)
+    urls = get_item_urls(html_content)
+    return urls
 
 
 def main():
     user_value = input("Enter the name of the desired product: ")
-    parse_connect(user_value)
-    get_item_urls("D:/PythonProjects/Wildb/Wild.html")
-    get_data("D:/PythonProjects/Wildb/Wild.html")
+    urls = parse_connect(user_value)
+    get_data(urls)
 
 
 if __name__ == '__main__':
